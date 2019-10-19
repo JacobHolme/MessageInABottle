1cd4576969e62327c54a04db1b44ded309bf3a77
Jacob Holme <jakeholme@icloud.com>
2019-10-19 05:56:44 -0400

    Message class created

---
+++ MessageInABottle/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  MessageInABottle
+//
+//  Created by Jacob Holme on 10/19/19.
+//  Copyright © 2019 Holme. All rights reserved.
+//
+
+import Foundation
+
+struct Message
+{
+    var sender: String
+    var message: String
+    var timeStamp: String
+}
