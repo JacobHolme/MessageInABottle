e7aaa2a4a62a66f811fa18c11e5febadf0c70d28
Joshua Holme <joshuaholme19@icloud.com>
2019-10-19 15:41:50 -0400

    Added the preliminary UI for the settings application. It seems stretched out now but we can take another look at the spacing after a navigation bar has been added to it.

--- MessageInABottle/SettingsViewController.swift
+++ MessageInABottle/SettingsViewController.swift
@@ -14,14 +14,14 @@ class SettingsViewController: UIViewController
     var backgroundActivityLabel = UILabel()
     var backgroundActivityDescription = UILabel()
     var backgroundActivityToggle = UISwitch()
-    var backgroundStackViewRowTop = UIStackView()
-    var backgroundStackViewRowFull = UIStackView()
+    var backgroundActivityStackViewRowTop = UIStackView()
+    var backgroundActivityStackViewRowFull = UIStackView()
     
     var locationSharingLabel = UILabel()
     var locationSharingDescription = UILabel()
     var locationSharingToggle = UISwitch()
-    var locationStackViewRowTop = UIStackView()
-    var locationStackViewRowFull = UIStackView()
+    var locationSharingStackViewRowTop = UIStackView()
+    var locationSharingStackViewRowFull = UIStackView()
     
     var batterySaverLabel = UILabel()
     var batterySaverDescription = UILabel()
@@ -35,8 +35,51 @@ class SettingsViewController: UIViewController
     {
         super.viewDidLoad()
 
-    
+        backgroundActivityLabel.text = "Background Activity"
+        backgroundActivityLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        backgroundActivityDescription.text = "This is a description of the Background Activity setting."
         
-        // Do any additional setup after loading the view.
+        backgroundActivityStackViewRowFull.axis = .vertical
+        backgroundActivityStackViewRowTop.addArrangedSubview(backgroundActivityLabel)
+        backgroundActivityStackViewRowTop.addArrangedSubview(backgroundActivityToggle)
+        backgroundActivityStackViewRowFull.addArrangedSubview(backgroundActivityStackViewRowTop)
+        backgroundActivityStackViewRowFull.addArrangedSubview(backgroundActivityDescription)
+        
+        locationSharingLabel.text = "Send Location"
+        locationSharingLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        locationSharingDescription.text = "This is a description of the Send Location setting."
+        
+        locationSharingStackViewRowFull.axis = .vertical
+        locationSharingStackViewRowTop.addArrangedSubview(locationSharingLabel)
+        locationSharingStackViewRowTop.addArrangedSubview(locationSharingToggle)
+        locationSharingStackViewRowFull.addArrangedSubview(locationSharingStackViewRowTop)
+        locationSharingStackViewRowFull.addArrangedSubview(locationSharingDescription)
+    
+        batterySaverLabel.text = "Battery Saver"
+        batterySaverLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        batterySaverDescription.text = "This is a description of the Battery Location setting."
+        
+        batterySaverStackViewRowFull.axis = .vertical
+        batterySaverStackViewRowTop.addArrangedSubview(batterySaverLabel)
+        batterySaverStackViewRowTop.addArrangedSubview(batterySaverToggle)
+        batterySaverStackViewRowFull.addArrangedSubview(batterySaverStackViewRowTop)
+        batterySaverStackViewRowFull.addArrangedSubview(batterySaverDescription)
+        
+        settingsStackView.axis = .vertical
+        settingsStackView.distribution = .fillEqually
+        settingsStackView.addArrangedSubview(backgroundActivityStackViewRowFull)
+        settingsStackView.addArrangedSubview(locationSharingStackViewRowFull)
+        settingsStackView.addArrangedSubview(batterySaverStackViewRowFull)
+        
+        view.addSubview(settingsStackView)
+        
+        settingsStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            settingsStackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            settingsStackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            settingsStackView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            settingsStackView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+        ])
+       
     }
 }
