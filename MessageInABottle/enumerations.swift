687badb3ba18ef23b15d7440f6d6cd3658cde873
Jacob Holme <jakeholme@icloud.com>
2019-10-19 02:21:22 -0400

    Added safety status enumeration

---
+++ MessageInABottle/enumerations.swift
@@ -0,0 +1,17 @@
+//
+//  enumerations.swift
+//  MessageInABottle
+//
+//  Created by Jacob Holme on 10/19/19.
+//  Copyright © 2019 Holme. All rights reserved.
+//
+
+import Foundation
+
+enum safetyStatus
+{
+    case safe
+    case stuck
+    case injuredNotSerious
+    case injuredNeedsAttention
+}
