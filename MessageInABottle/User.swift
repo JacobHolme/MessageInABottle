9b3f38431c03f79b776d28e759d34b53b71253f4
Jacob Holme <jakeholme@icloud.com>
2019-10-19 02:19:19 -0400

    Added user class

---
+++ MessageInABottle/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  MessageInABottle
+//
+//  Created by Jacob Holme on 10/19/19.
+//  Copyright © 2019 Holme. All rights reserved.
+//
+
+import Foundation
+
+struct User
+{
+    var firstName: String
+    var lastName: String
+    var status: safetyStatus
+}
--- MessageInABottle/ViewController.swift
+++ MessageInABottle/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
